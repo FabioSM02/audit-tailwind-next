d3cceb6cd13e06ace8574da5854589780f0e2dec
FabioSM02 <fctrabnet@gmail.com>
2023-02-23 00:29:55 -0300

    adjust files

--- src/components/fields.tsx
+++ src/components/fields.tsx
@@ -8,24 +8,29 @@ interface FieldProps {
     maxLenght?: number;
     required: boolean;
     disable?: boolean;
+    pattern?: string;
 }
 
-export function Field( { id, placeholder, type, maxLenght, minLenght, required, disable } : FieldProps ){
+export function Field( { id, placeholder, type, maxLenght, minLenght, required, disable, pattern } : FieldProps ){
     return (
-        // <div className="flex">
-        //     <label className="text-indigo-500 h-[20px] px-2 bg-[#1C1C1C] text-xs relative top-[12px] left-[160px]">
-        //         {placeholder}
-        //     </label>
+        <div className="flex m-0 p-0">
+            {/* <p className="text-indigo-500 h-[20px] px-2 bg-[#1C1C1C] text-xs relative top-[12px] left-[160px]">
+                {placeholder}
+            </p> */}
+            {/* <label className="text-indigo-500 h-[20px] px-2 bg-[#1C1C1C] text-xs relative top-[12px] left-[160px]">
+                {placeholder}
+            </label> */}
             <input 
                 id={id}
                 type={type} 
                 maxLength={maxLenght} 
                 minLength={minLenght} 
-                className="w-72 max-w-xs h-10 px-4 my-5 mx-5 text-[16px] rounded-md bg-transparent border-[.8px] border-indigo-900 focus:border-indigo-500 focus:outline-none" 
+                className="w-72 h-10 px-4 m-5 text-[16px] rounded-md bg-transparent border-[.2px] border-indigo-900 focus:border-indigo-500 focus:outline-none max-xl:max-w-xl" 
                 placeholder={placeholder} 
                 required={required}
                 disabled={disable}
+                pattern={pattern}
             />
-        // </div>
+        </div>
     );
 }
--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Campo } from "../components/campo";
+// import { Campo } from "../components/campo";
 import { Field } from "../components/fields";
 
 export function Cadastro(){
@@ -8,24 +8,24 @@ export function Cadastro(){
             <p className="text-indigo-500 font-lexend font-bold my-10 text-2xl">
                 Cadastrar empresa
             </p>
-            <form action="" method="get" className="max-w-[1280px] mb-8 border-[.2px] border-blue-600">
-                <fieldset className="flex justify-center">
-                    <Field id="companyName" type="text" placeholder="Nome da empresa" required disable />
-                    <Field id="cpnj" type="" placeholder="CNPJ" required maxLenght={18} minLenght={14} />
-                    <Field id="corporateRepresentative" type="text" placeholder="Nome do representante" required />
+            <form action="" method="get" className="max-w-[1280px] flex-wrap mb-8">
+                <fieldset className="flex items-center justify-center">
+                    <Field id="companyName" type="text" placeholder="Empresa" required disable />
+                    <Field id="cpnj" type="text" placeholder="CNPJ" required maxLenght={18} minLenght={14} pattern="00.000.000/0000-00" />
+                    <Field id="corporateRepresentative" type="text" placeholder="Representante" required />
                 </fieldset>
-                <fieldset className="flex justify-center">
+                <fieldset className="flex items-center justify-center">
                     <Field id="" type="text" placeholder="Cargo" required />
-                    <Field id="branch" type="text" placeholder="Ramo" required disable />
+                    <Field id="branch" type="text" placeholder="Segmento" required />
                     <Field id="" type="text" placeholder="Porte da empresa" required/>
                 </fieldset>
                 <fieldset className="flex justify-center">
-                    <Field id="phone" type="" placeholder="Número de telefone" required />
-                    <Field id="cep" type="" placeholder="CEP" required />
+                    <Field id="phone" type="tel" placeholder="Número de telefone" required minLenght={11} maxLenght={11} pattern="" />
+                    <Field id="cep" type="" placeholder="CEP" required pattern="00.000-000" minLenght={10} maxLenght={10} />
                     <Field id="street" type="" placeholder="Rua" required disable />
                 </fieldset>
                 <fieldset className="flex justify-center">
-                    <Field id="number" type="" placeholder="Número" required />
+                    <Field id="number" type="" placeholder="Número" required maxLenght={5} />
                     <Field id="city" type="" placeholder="Cidade" required disable />
                     <Field id="state" type="" placeholder="Estado" required disable />
                 </fieldset>
@@ -39,7 +39,7 @@ export function Cadastro(){
                     <Field id="checkPassword" type="password" placeholder="Confirmar senha" maxLenght={30} minLenght={8} required />
                 </fieldset> */}
 
-                <fieldset className="flex flex-col items-center justify-center">
+                <fieldset className="flex items-center justify-center">
                     <button className="flex justify-center items-center h-12 w-40 border-[.4px] bg-indigo-100 text-indigo-500 font-bold  border-indigo-500 rounded-md focus:outline-none hover:bg-indigo-300 hover:text-indigo-800 hover:transition-colors">
                         Cadastrar
                     </button>
